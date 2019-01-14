053e707981c42e92c8a90334cdbbac703c683f57
Jake Moshenko <jake.moshenko@gmail.com>
2019-01-14 17:22:55 -0500

    Fix the packaging.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jake Moshenko',
     author_email='jacob.moshenko@gmail.com',
     packages=['pagerditty'],
-    scripts=['bin/generate-report.py'],
+    scripts=['bin/generate_report.py'],
     url='http://pypi.python.org/pypi/PagerDitty/',
     license='LICENSE.txt',
     description='Useful scripts for generating oncall reports from PagerDuty.',
